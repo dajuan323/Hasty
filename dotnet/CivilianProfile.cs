2f7dad0ef80caf2ec57534d6b6f9cb175682cf2a
dajuan323 <macaro.martin@gmail.com>
2023-04-13 16:30:13 -0400

    initial push

---
+++ dotnet/CivilianProfile.cs
@@ -0,0 +1,20 @@
+﻿using Sabio.Models.Domain.Users;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Sabio.Models.Domain.CivilianProfiles
+{
+    public class CivilianProfile
+    {
+        public int Id { get; set; }
+        public BaseUser User { get; set; }
+        public int MonthlyIncome { get; set; }
+        public DateTime MoveInDate { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
