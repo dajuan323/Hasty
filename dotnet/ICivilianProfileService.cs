e44c4e16f7dfe3415cb544329fed8f932d2289c4
Da'Juan <83599259+dajuan323@users.noreply.github.com>
2023-06-14 15:26:02 -0400

    Update ICivilianProfileService.cs

--- dotnet/ICivilianProfileService.cs
+++ dotnet/ICivilianProfileService.cs
@@ -1,17 +1,11 @@
-﻿using Hasty.Models;
-using Hasty.Models.Domain.CivilianProfiles;
-using Hasty.Models.Domain.MilitaryProfile;
-using Hasty.Models.Requests.CivilianProfiles;
 
-namespace Hasty.Services.Interfaces
+public interface ICivilianProfileService
 {
-    public interface ICivilianProfileService
-    {
-        int Add(CivilianProfileAddRequest model, int userId);
-        void Delete(int id);
-        CivilianProfile GetById(int id);
-        Paged<CivilianProfile> GetPaged(int pageIndex, int pageSize);
-        Paged<CivilianProfile> SearchPaginated(int pageIndex, int pageSize, string query);
-        void Update(CivilianProfileUpdateRequest model, int userId);
-    }
+    int Add(CivilianProfileAddRequest model, int userId);
+    void Delete(int id);
+    CivilianProfile GetById(int id);
+    Paged<CivilianProfile> GetPaged(int pageIndex, int pageSize);
+    Paged<CivilianProfile> SearchPaginated(int pageIndex, int pageSize, string query);
+    void Update(CivilianProfileUpdateRequest model, int userId);
 }
+
